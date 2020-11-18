d044d23de2ff9862afcb4fa0de924aa3c5bba378
michelescarlato <michele.scarlato@gmail.com>
2020-11-18 12:52:28 +0900

    Update bigchaindb_node_config_after_clone.sh

--- bigchaindb_node_config_after_clone.sh
+++ bigchaindb_node_config_after_clone.sh
@@ -1,14 +1,16 @@
+#BigchainDB
+
+echo "Running sudo pip3 install bigchaindb==2.2.1"
+sudo pip3 install bigchaindb==2.2.1
+echo "waiting 5 seconds.."
+sleep 5
 
 echo "Running bigchaindb configure"
 bigchaindb configure
 echo "waiting 5 seconds.."
 sleep 5
 
-echo "Running bigchaindb-monit-config"
-bigchaindb-monit-config
-echo "waiting 5 seconds.."
-sleep 5
-
+#mongodb
 echo "Running sudo apt install -y mongodb"
 sudo apt install -y mongodb
 echo "waiting 15 seconds.."
@@ -19,11 +21,18 @@ systemctl status mongodb
 echo "waiting 5 seconds.."
 sleep 5
 
+#monit
 echo "Running bigchaindb-monit-config"
 bigchaindb-monit-config
 echo "waiting 5 seconds.."
 sleep 5
 
+echo "Running bigchaindb-monit-config"
+bigchaindb-monit-config
+echo "waiting 5 seconds.."
+sleep 5
+
+#tendermint
 echo "Running tendermint init"
 tendermint init
 echo "waiting 5 seconds.."
