75fdce65a06eecfff98398f9ea6b51922734ad6d
michelescarlato <michele.scarlato@gmail.com>
2020-12-02 13:30:02 +0900

    Update VM_snapshot_flooder_after_1st_boot.sh

--- VM_snapshot_flooder_after_1st_boot.sh
+++ VM_snapshot_flooder_after_1st_boot.sh
@@ -2,7 +2,7 @@
 cd
 pwd
 npm install bigchaindb-driver@4.1.0
-git clone https://github.com/michelescarlato/e-voting_Blockchain.git
-cd e-voting_Blockchain/tests/
+git clone https://github.com/michelescarlato/blockchainETIS.git
+cd blockchainETIS/tests/
 chmod +x TPS_bash_script.sh
 ./TPS_bash_script.sh
